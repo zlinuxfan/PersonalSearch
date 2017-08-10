cd2e44f7bce5132e52bef7b0c8997d3d874dffcf
Sergey Chikunkov <sergey.chikunkov@comodo.od.ua>
2017-08-10 11:55:27 +0300

    Fix uppercase in link element.

--- src/Utils/Utilities.java
+++ src/Utils/Utilities.java
@@ -26,7 +26,7 @@ public class Utilities {
 
     public static String toTransliteration(String string) {
         final char[] ru = {' ', 'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ч', 'Ц', 'Ш', 'Щ', 'Э', 'Ю', 'Я', 'Ы', 'Ъ', 'Ь', 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ч', 'ц', 'ш', 'щ', 'э', 'ю', 'я', 'ы', 'ъ', 'ь'};
-        final String[] en = {" ", "A", "B", "V", "G", "D", "E", "Jo", "Zh", "Z", "I", "J", "K", "L", "M", "N", "O", "P", "R", "S", "T", "U", "F", "H", "Ch", "C", "Sh", "Csh", "E", "Ju", "Ja", "Y", "", "", "a", "b", "v", "g", "d", "e", "jo", "zh", "z", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "f", "h", "ch", "c", "sh", "csh", "e", "ju", "ja", "y", "", ""};
+        final String[] en = {" ", "a", "b", "v", "g", "d", "e", "jo", "zh", "z", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "f", "h", "ch", "c", "sh", "csh", "e", "ju", "ja", "Y", "", "", "a", "b", "v", "g", "d", "e", "jo", "zh", "z", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "f", "h", "ch", "c", "sh", "csh", "e", "ju", "ja", "y", "", ""};
         final StringBuilder response = new StringBuilder(string.length());
         final HashMap<Character, String> table = new HashMap<>();
 
@@ -43,7 +43,7 @@ public class Utilities {
     }
 
     public static Document getDocument(String url) throws IOException {
-        Document document = null;
+        Document document;
 
             document = Jsoup
                     .connect(url)
